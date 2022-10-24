818264c99fffcd41979535ad5b1f9b28f49584c2
Sarvar <arifdjanovsarvar@gmail.com>
2022-10-24 13:29:23 +0500

    Phase 3
    added SoundPool for sounds
    added a unit test for SoundViewModel
    implemented a play function for sounds

--- app/src/main/java/com/example/beatbox_kotlin/Sound.kt
+++ app/src/main/java/com/example/beatbox_kotlin/Sound.kt
@@ -2,6 +2,6 @@ package com.example.beatbox_kotlin
 
 private const val WAV = ".wav"
 
-class Sound(val assetPath: String) {
+class Sound(val assetPath: String, var soundId: Int? = null) {
     val name = assetPath.split("/").last().removeSuffix(WAV)
 }
